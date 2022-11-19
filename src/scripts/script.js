696cb41f28c1d90a77028d49f475b877209b918b
Devon Vendetti <d.vendetti@gmail.com>
2022-11-19 02:13:00 -0800

    removed all swiper and typed nonsense.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,21 +1,4 @@
-$(document).ready(function () {
-  // TOGGLE MENU - NAVBAR
-  $('.menu-btn').click(function () {
-    $('.navbar .menu').toggleClass('active');
-    $('.menu-btn i').toggleClass('active');
-  });
-
-  // TYPING ANIMATION SCRIPT
-  let typed1 = new Typed('.typed', {
-    strings: [
-      'Model',
-      'Athlete',
-      'Fit-preneur',
-      'Fitspiration Bootcamp Queen',
-      'your Miss Nevada',
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  document.querySelector('.navbar .menu').classList.toggle('active');
+  document.querySelector('.menu-btn i').classList.toggle('active');
 });
